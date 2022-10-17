98103bab99f4c029868c80acd5f13c9e00818f40
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-16 21:50:16 -0500

    create auth structure

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatFormFieldModule, MatInputModule],
 })
 export class AuthModule {}
