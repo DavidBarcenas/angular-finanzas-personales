ec9f52d65b8b071a1dbd265ccc19f83eaa103e1c
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-24 12:18:25 -0500

    update auth router and registration form

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
     children: [
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
-      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: 'acceder', pathMatch: 'full' },
+      { path: 'acceder', component: LoginComponent },
+      { path: 'registro', component: RegisterComponent },
     ],
   },
 ];
