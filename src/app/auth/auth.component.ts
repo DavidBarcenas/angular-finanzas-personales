5f89925c0eb320952a9ee798f2fa7e05be8e0096
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-24 22:30:30 -0500

    refactor login and register

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent implements OnDestroy {
+  title = '';
+  subscriber: Subscription;
+
+  constructor(private router: Router) {
+    this.subscriber = this.router.events.subscribe((e: Event) => {
+      if (e instanceof NavigationEnd) {
+        this.title = e['url'] === '/registro' ? 'Crea tu cuenta' : 'Iniciar Sesión';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
+}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  constructor(private authService: AuthService) {}
+}
